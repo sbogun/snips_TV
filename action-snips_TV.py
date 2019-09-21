ba8714f3d85754c3c5449c5667275b13058d4d2c
Stefan Bogun <sbogun@team.mobile.de>
2019-09-21 19:37:45 +0200

    action-snips_TV.py: add station selection (WIP)

--- action-snips_TV.py
+++ action-snips_TV.py
@@ -7,8 +7,50 @@ import json
 
 
 MQTT_TOPIC = 'cmnd/sonoff/IRSend'
-MQTT_MSG = json.dumps({"Protocol": "NEC", "Bits": 32, "Data": "0x616E02FD"});
 MQTT_BROKER_ADDRESS = 'localhost:1883'
+
+MQTT_MSG = {}
+MQTT_MSG['power_on_AMP'] = json.dumps({"Protocol": "NEC", "Bits": 32, "Data": "0x616E02FD"})
+MQTT_MSG['0'] = json.dumps({"Protocol":"SAMSUNG","Bits":32,"Data":"0xE0E08877"})
+MQTT_MSG['1'] = json.dumps({"Protocol":"SAMSUNG","Bits":32,"Data":"0xE0E020DF"})
+MQTT_MSG['2'] = json.dumps({"Protocol":"SAMSUNG","Bits":32,"Data":"0xE0E0A05F"})
+MQTT_MSG['3'] = json.dumps({"Protocol":"SAMSUNG","Bits":32,"Data":"0xE0E0609F"})
+MQTT_MSG['4'] = json.dumps({"Protocol":"SAMSUNG","Bits":32,"Data":"0xE0E010EF"})
+MQTT_MSG['5'] = json.dumps({"Protocol":"SAMSUNG","Bits":32,"Data":"0xE0E0906F"})
+MQTT_MSG['6'] = json.dumps({"Protocol":"SAMSUNG","Bits":32,"Data":"0xE0E050AF"})
+MQTT_MSG['7'] = json.dumps({"Protocol":"SAMSUNG","Bits":32,"Data":"0xE0E030CF"})
+MQTT_MSG['8'] = json.dumps({"Protocol":"SAMSUNG","Bits":32,"Data":"0xE0E0B04F"})
+MQTT_MSG['9'] = json.dumps({"Protocol":"SAMSUNG","Bits":32,"Data":"0xE0E0708F"})
+
+PROGRAM_KEY_MAP = {}
+PROGRAM_KEY_MAP['daserste'] = [1]
+PROGRAM_KEY_MAP['zdf'] = [2]
+PROGRAM_KEY_MAP['arte'] = [3]
+PROGRAM_KEY_MAP['sat1'] = [4]
+PROGRAM_KEY_MAP['rtl'] = [5]
+PROGRAM_KEY_MAP['rtl2'] = [6]
+PROGRAM_KEY_MAP['pro7'] = [7]
+PROGRAM_KEY_MAP['vox'] = [8]
+PROGRAM_KEY_MAP['sixx'] = [9]
+PROGRAM_KEY_MAP['tele5'] = [1,0]
+PROGRAM_KEY_MAP['kabel1'] = [1,1]
+PROGRAM_KEY_MAP['servustv'] = [1,2]
+PROGRAM_KEY_MAP['pro7maxx'] = [1,3]
+PROGRAM_KEY_MAP['one'] = [1,4]
+PROGRAM_KEY_MAP['superrtl'] = [1,5]
+PROGRAM_KEY_MAP['nitro'] = [1,6]
+PROGRAM_KEY_MAP['sat1gold'] = [1,7]
+PROGRAM_KEY_MAP['rtlplus'] = [1,8]
+PROGRAM_KEY_MAP['rbb'] = [1,9]
+PROGRAM_KEY_MAP['ndr'] = [2,0]
+PROGRAM_KEY_MAP['hrfernsehen'] = [2,1]
+PROGRAM_KEY_MAP['eurosport1'] = [3,6]
+PROGRAM_KEY_MAP['ntv'] = [4,1]
+PROGRAM_KEY_MAP['radio1'] = [6,5]
+PROGRAM_KEY_MAP['cosmo'] = [1,0,7]
+PROGRAM_KEY_MAP['jamfm'] = [1,2,0]
+PROGRAM_KEY_MAP['fluxfm'] = [1,2,1]
+PROGRAM_KEY_MAP['paradiso'] = [1,2,2]
 
 
 
@@ -20,6 +62,9 @@ def subscribe_intent_callback(hermes, intent_message):
         mqttc.connect('localhost', 1883)
         mqttc.publish(MQTT_TOPIC, payload = MQTT_MSG)
         mqttc.disconnect()
+
+    elif intentname == "sbogun:TV_program":
+        station_name = intent_message.slots.station_name.first()
 
 
 
